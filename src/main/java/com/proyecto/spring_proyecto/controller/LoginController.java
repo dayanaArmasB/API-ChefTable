cf77e5be5c00eb86274ecf7009262f39fafd5508
Dayana <dyarmasb@hotmail.com>
2025-10-03 11:33:35 -0500

    implementacion de register

--- src/main/java/com/proyecto/spring_proyecto/controller/LoginController.java
+++ src/main/java/com/proyecto/spring_proyecto/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.proyecto.spring_proyecto.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto.spring_proyecto.application.interfaces.IJwtService;
 import com.proyecto.spring_proyecto.application.interfaces.IUserService;
-import com.proyecto.spring_proyecto.auth.dto.LoginLegacyResponse;
 import com.proyecto.spring_proyecto.auth.dto.LoginRequest;
 import com.proyecto.spring_proyecto.auth.dto.LoginResponse;
+import com.proyecto.spring_proyecto.core.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,13 +24,17 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
 
     private final IUserService userService;
-    private final IJwtService jwt;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         return userService.authenticate(request.address(), request.password())
                 .map(token -> ResponseEntity.ok(new LoginResponse(request.address(), token)))
                 .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+    
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return ResponseEntity.ok(userService.save(user));
     }
 
 }
